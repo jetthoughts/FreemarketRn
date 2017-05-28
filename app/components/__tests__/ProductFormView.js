77af4518539aaa7c382b9d32e58f8be453910838
obliviusm <obliviusm@gmail.com>
2017-05-28 13:05:05 +0300

    Update test for form

--- app/components/__tests__/ProductFormView.js
+++ app/components/__tests__/ProductFormView.js
@@ -1,16 +1,25 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import MockDate from 'mockdate';
 
 import ProductFormView from '../ProductFormView';
+import categories from '../../constants/categories'
+import { categoriesToObject } from '../../lib/helpers';
 
 it('renders successfully', () => {
+  MockDate.set('1/1/2000');
+
   const tree = renderer.create(
     <ProductFormView
+      categories={categoriesToObject(categories)}
       onPress={() => true}
       onBackPress={() => true}
+      loadCategories={() => true}
     />,
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
+
+  MockDate.reset();
 });
